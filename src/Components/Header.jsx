96eca69e156d59dbd4a3c97639e384d155d1ecaa
tanishpophale930 <tanishpophale930@gmail.com>
2024-10-14 16:17:51 +0530

    Adjusting the notifications and messages dropdown width

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -62,10 +62,10 @@ const Header = ({Sidebartoggle, setSidebartoggle}) => {
 								leaveFrom="opacity-100 translate-y-0"
 								leaveTo="opacity-0 translate-y-1"
 							>
-								<Popover.Panel className="absolute right-0 z-10 mt-1 transform w-80">
-									<div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
-										<strong className="text-gray-700 font-medium">Messages</strong>
-										<div className="mt-2 py-1 text-sm">No messages received.</div>
+								<Popover.Panel className="absolute right-0 z-10 mt-1 transform w-60 md:w-72">
+									<div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-4 py-2.5">
+										<strong className="text-gray-700 font-semibold text-lg">Messages</strong>
+										<div className="mt-2 py-1 text-md">No messages received.</div>
 									</div>
 								</Popover.Panel>
 							</Transition>
@@ -93,10 +93,10 @@ const Header = ({Sidebartoggle, setSidebartoggle}) => {
 								leaveFrom="opacity-100 translate-y-0"
 								leaveTo="opacity-0 translate-y-1"
 							>
-								<Popover.Panel className="absolute right-0 z-10 mt-1 transform w-60">
-									<div className="bg-white rounded-md shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
-										<strong className="text-gray-700 font-medium">Notifications</strong>
-										<div className="mt-2 py-1 text-sm"> No notifications received.</div>
+								<Popover.Panel className="absolute right-0 z-10 mt-1 transform w-60 md:w-72">
+									<div className="bg-white rounded-md shadow-md ring-1 ring-black ring-opacity-5 px-4 py-2.5">
+										<strong className="text-gray-700 font-semibold text-lg">Notifications</strong>
+										<div className="mt-2 py-1 text-md"> No notifications received.</div>
 									</div>
 								</Popover.Panel>
 							</Transition>
