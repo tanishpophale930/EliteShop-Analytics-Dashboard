8d3eaf71ca67d4b598e1176bec79707d119f1d48
tanishpophale930 <tanishpophale930@gmail.com>
2024-10-14 17:21:54 +0530

    Hidded Transaction chart on mobile screens since it wasnot getting adjusted to small screens so it is hidded on small screens

--- src/Components/Dashboard/TransactionsChart.jsx
+++ src/Components/Dashboard/TransactionsChart.jsx
@@ -4,9 +4,9 @@ import { TransactionsChartData } from './TransactionsChartData';
 
 const TransactionsChart = () => {
   return (
-    <div className='h-[100rem]  md:h-[23rem] flex flex-col flex-1 gap-3 py-4 bg-white border border-gray-200 rounded-sm'>
+    <div className='hidden md:h-[23rem] md:flex flex-col flex-1 gap-3 py-4 bg-white border border-gray-200 rounded-sm'>
         <p className="text-gray-800  text-xl font-bold px-4 tracking-wide text-center font-roboto">Transactions</p>
-        <div className='px-0 md:px-2 lg:px-3 w-full flex-1 text-xs md:text-xs lg:text-sm'>
+        <div className='md:px-2 lg:px-3 w-full flex-1 md:text-xs lg:text-sm'>
             <ResponsiveContainer width="100%" height="100%">
               <BarChart
                   data={TransactionsChartData}
