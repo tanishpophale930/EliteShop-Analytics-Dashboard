8ce569478871c7d63b453ce603dcb4da0b915f8a
tanishpophale930 <tanishpophale930@gmail.com>
2024-10-14 17:02:36 +0530

    Adjusting the Transactions Chart to mobile screens

--- src/Components/Dashboard/TransactionsChart.jsx
+++ src/Components/Dashboard/TransactionsChart.jsx
@@ -4,14 +4,14 @@ import { TransactionsChartData } from './TransactionsChartData';
 
 const TransactionsChart = () => {
   return (
-    <div className='h-[23rem] md:h-[20rem] sm:h-[15rem] flex flex-col flex-1 gap-3 py-4 bg-white border border-gray-200 rounded-sm'>
+    <div className='h-[23rem] flex flex-col flex-1 gap-3 py-4 bg-white border border-gray-200 rounded-sm'>
         <p className="text-gray-800 text-xl font-bold px-4 tracking-wide text-center font-roboto">Transactions</p>
         <div className='px-3 w-full flex-1 text-sm'>
             <ResponsiveContainer width="100%" height="100%">
               <BarChart
                   data={TransactionsChartData}
                   margin={{
-                      top: 10,
+                      top: 20,
                       right: 10,
                       left: -10,
                       bottom: 0
